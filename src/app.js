f824b141d78cddb1a12769be48deb28d68817a9d
Michael <mmbmf.mm@gmail.com>
2020-05-16 12:14:28 -0500

    minor comment changes

--- src/app.js
+++ src/app.js
@@ -21,15 +21,15 @@ app.get("/", (req, res) => {
   res.send("Hello, Todos!");
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+// app.use(function errorHandler(error, req, res, next) {
+//   let response;
+//   if (NODE_ENV === "production") {
+//     response = { error: { message: "server error" } };
+//   } else {
+//     console.error(error);
+//     response = { message: error.message, error };
+//   }
+//   res.status(500).json(response);
+// });
 
 module.exports = app;
