ee66cb61afcf50dff0bf3d8d2b4947f0d6a38f46
Michael <mmbmf.mm@gmail.com>
2020-05-15 20:43:08 -0500

    working crud api

--- src/todos/todos-router.js
+++ src/todos/todos-router.js
@@ -12,7 +12,7 @@ todosRouter.route("/").post(jsonBodyParser, async (req, res) => {
     const { description } = req.body;
     const newTodo = { description };
 
-    await TodosService.insertToDo(req.app.get("db"), newTodo).then((todo) => {
+    await TodosService.insertTodo(req.app.get("db"), newTodo).then((todo) => {
       res.status(201).json(todo);
     });
   } catch (error) {
@@ -24,7 +24,9 @@ todosRouter.route("/").post(jsonBodyParser, async (req, res) => {
 
 todosRouter.route("/").get(async (req, res) => {
   try {
-    ToDosService;
+    await TodosService.getAllTodos(req.app.get("db")).then((todos) =>
+      res.json(todos)
+    );
   } catch (error) {
     console.error(error.message);
   }
@@ -32,8 +34,50 @@ todosRouter.route("/").get(async (req, res) => {
 
 //get a todo
 
+todosRouter.route("/todo/:todo_id").get(async (req, res) => {
+  try {
+    const { todo_id } = req.params;
+
+    await TodosService.getTodoById(req.app.get("db"), todo_id).then((todo) =>
+      res.json(todo)
+    );
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 //update a todo
 
+todosRouter.route("/todo/:todo_id").patch(jsonBodyParser, async (req, res) => {
+  try {
+    const { todo_id } = req.params;
+    const { description } = req.body;
+    const newDescription = { description };
+
+    await TodosService.updateTodo(
+      req.app.get("db"),
+      todo_id,
+      newDescription
+    ).then((todo) => {
+      res.json(todo);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 //delete a todo
+
+todosRouter.route("/todo/:todo_id").delete(async (req, res) => {
+  try {
+    const { todo_id } = req.params;
+
+    await TodosService.deleteTodo(req.app.get("db"), todo_id).then(() => {
+      res.json("Todo deleted");
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = todosRouter;
--- src/todos/todos-service.js
+++ src/todos/todos-service.js
@@ -4,11 +4,28 @@ const TodosService = {
       .insert(newTodo)
       .into("todos")
       .returning("*")
-      .then(([todo]) => todo);
+      .then((todo) => todo);
   },
 
-  getAllTodos() {
-    console.log("Getting all todos");
+  getAllTodos(db) {
+    return db.from("todos").select("*");
+  },
+
+  getTodoById(db, todo_id) {
+    return db.from("todos").select("*").where("todo_id", todo_id);
+  },
+
+  updateTodo(db, todo_id, newDescription) {
+    return db
+      .from("todos")
+      .where({ todo_id })
+      .update(newDescription)
+      .returning("*")
+      .then((newTodo) => newTodo);
+  },
+
+  deleteTodo(db, todo_id) {
+    return db.from("todos").where({ todo_id }).delete();
   },
 };
 
